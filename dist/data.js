cca13f51ee4933dfab388e0cb5893d3d229e5028
Caleb Fox <g00327455@gmit.ie>
2020-03-30 16:43:55 +0100

    deploy

---
+++ dist/data.js
@@ -0,0 +1,37 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  products: [{
+    _id: '1',
+    name: 'BMW520',
+    category: 'BMW',
+    image: '/images/car1.jpeg',
+    price: 10000,
+    brand: 'BMW',
+    mileage: '100000',
+    countInStock: 6
+  }, {
+    _id: '2',
+    name: 'AudiA4',
+    category: 'Audi',
+    image: '/images/audia4.jpeg',
+    price: 15000,
+    brand: 'Audi',
+    mileage: '120000',
+    countInStock: 6
+  }, {
+    _id: '3',
+    name: 'Renault Clio',
+    category: 'Reno',
+    image: '/images/clio.jpeg',
+    price: 5000,
+    brand: 'Renault',
+    mileage: '150000',
+    countInStock: 6
+  }]
+};
+exports["default"] = _default;
